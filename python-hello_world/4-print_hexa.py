0f733327d150cf7486325429c2a565c8c64ffb26
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-15 16:49:18 +0100

    4. Hexadecimal printing

---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,5 @@
+# Print numbers from 0 to 98 in decimal and hexadecimal
+for num in range(99):  # Range includes 0 to 98
+    decimal = num
+    hexadecimal = hex(num)
+    print(f"Decimal: {decimal}, Hexadecimal: {hexadecimal}")
