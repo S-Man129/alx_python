e82b07c06b2de24360484b49449a467e8bb4916f
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-15 17:25:24 +0100

    5. 00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,5 @@
 # Print numbers from 0 to 99 in ascending order with two digits, separated by ", "
-for num in range(100):  # Range includes 0 to 99
-    # formatted_num = ("{:02d}, ".format(num))
-    # # Print the formatted number followed by a comma and a space
-    # print(formatted_num, end=" ")
+for num in range(100):
     if num < 99:
         print("{:02d}, ".format(num), end="")
     else:
