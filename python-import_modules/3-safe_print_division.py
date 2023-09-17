ddc794a2a40be37bb8e8878da19c5520a51e8a05
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-17 08:27:15 +0100

    3. Integers division with debug

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed")
+        return None
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
