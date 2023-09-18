ebed9d9bf687b43cba02c3c8fdadfec8a8f2aecb
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-18 11:39:00 +0100

    Error fix

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,25 +2,15 @@
 """ Class Rectangle """
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
+class BaseGeometry:
+    def area(self):
+        raise NotImplementedError("area() method not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError(f"{name} must be a positive integer")
 
 class Rectangle(BaseGeometry):
-    """ Class Rectangle """
     def __init__(self, width, height):
-        """ init method """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-    
-    def __init_subclass__(cls):
-        cls.__init_subclass__ = lambda *args, **kwargs: None
-
-    def __dir__(self):
-        # Get the default list of attributes and methods
-        default_dir = super().__dir__()
-
-        # Exclude '__init_subclass__' if it's in the default_dir
-        if '__init_subclass__' in default_dir:
-            default_dir.remove('__init_subclass__')
-
-        return default_dir
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
