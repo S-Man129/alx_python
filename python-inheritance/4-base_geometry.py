caf8155ee7e77efe53e2d911eaf7f9821073bf5e
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-18 11:09:12 +0100

    Correction of "5. Integer validator"

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -7,3 +7,16 @@ class BaseGeometry:
     def area(self):
         """ method area """
         raise Exception("area() is not implemented")
+    
+    def __init_subclass__(cls):
+        cls.__init_subclass__ = lambda *args, **kwargs: None
+
+    def __dir__(self):
+        # Get the default list of attributes and methods
+        default_dir = super().__dir__()
+
+        # Exclude '__init_subclass__' if it's in the default_dir
+        if '__init_subclass__' in default_dir:
+            default_dir.remove('__init_subclass__')
+
+        return default_dir
