e51995f2a7ca2b5c15b61688293c86f8724bdcba
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-18 10:21:34 +0100

    3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,3 +5,4 @@ class BaseGeometry:
     """ Empty class BaseGeometry """
     def __init_subclass__(cls):
         """ excluding __init_subclass__ """
+        cls.__init_subclass__ = lambda *args, **kwargs: None
