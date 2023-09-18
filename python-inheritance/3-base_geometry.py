815a7909b97b51abc474cd88185c5ba4e5101d66
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-18 10:26:59 +0100

    3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 """ Empty class BaseGeometry """
 
 class BaseGeometry:
     """ Empty class BaseGeometry """
-    def __init_subclass__(cls):
-        """ excluding __init_subclass__ """
-        cls.__init_subclass__ = lambda *args, **kwargs: None
+    def __dir__(self):
+        """ defining directory self """
+        return [attr for attr in dir(self) if attr != '__init_subclass__']
