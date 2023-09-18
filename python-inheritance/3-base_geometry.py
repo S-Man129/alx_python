904daa6b28cbcbaf43364f50985ad22fa9bd768d
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-18 10:56:49 +0100

    3. Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,15 +4,17 @@
 
 class BaseGeometry:
     """ BaseGeometry Empty class"""
-    def __init_subclass__(cls):
-        cls.__init_subclass__ = lambda *args, **kwargs: None
-
     def __dir__(self):
-        # Get the default list of attributes and methods
-        default_dir = super().__dir__()
+        return [attr for attr in dir(self) if attr != '__init_subclass__']
+    # def __init_subclass__(cls):
+    #     cls.__init_subclass__ = lambda *args, **kwargs: None
 
-        # Exclude '__init_subclass__' if it's in the default_dir
-        if '__init_subclass__' in default_dir:
-            default_dir.remove('__init_subclass__')
+    # def __dir__(self):
+    #     # Get the default list of attributes and methods
+    #     default_dir = super().__dir__()
 
-        return default_dir
+    #     # Exclude '__init_subclass__' if it's in the default_dir
+    #     if '__init_subclass__' in default_dir:
+    #         default_dir.remove('__init_subclass__')
+
+    #     return default_dir
