b25c45fd91e212f8d5e10871d2995dea3f0bf97e
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-20 08:56:56 +0100

    0. What is my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,12 +3,26 @@
     https://intranet.hbtn.io/status
 """
 
-if __name__ == "__main__":
-    import urllib.request
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+# if __name__ == "__main__":
+#     import urllib.request
+#     url = "https://intranet.hbtn.io/status"
+#     with urllib.request.urlopen(url) as response:
+#         body = response.read()
+#         print("Body response:")
+#         print("\t- type: {}".format(type(body)))
+#         print("\t- content: {}".format(body))
+#         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+import requests
+
+url = "https://alu-intranet.hbtn.io/status"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+else:
+    print("Error: Unable to fetch URL. Status code:", response.status_code)
