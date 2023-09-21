d7860cfff018e07e00741827a491b8491bd60510
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-21 18:33:09 +0100

    0. Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
 """ Import flask framework"""
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Defining the module Hello_hbnb """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
