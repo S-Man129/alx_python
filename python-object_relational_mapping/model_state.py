b17fee2eb820a889c3787d1851980882bdf89f89
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-24 15:04:41 +0100

    model state fetch

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python3
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """
+    State class represents a table in the MySQL database.
+    
+    Attributes:
+        id (int): An auto-generated unique identifier for the state.
+        name (str): A string representing the name of the state.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-# Example usage:
+# If you want to test the script, you can include the following block of code
 if __name__ == '__main__':
     from sqlalchemy import create_engine
 
-    # Create an SQLAlchemy engine and bind it to the MySQL database
-    engine = create_engine('mysql://username:password@localhost:3306/database_name')
+    # Define the MySQL database connection URL (replace with your credentials)
+    db_url = 'mysql://username:password@localhost:3306/database_name'
 
-    # Import all classes that inherit from Base before calling create_all
+    # Create the database engine and create the 'states' table
+    engine = create_engine(db_url, echo=True)
     Base.metadata.create_all(engine)
