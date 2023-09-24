aa4f359588fdd4b2ec13f64c6e0121237c917d38
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-24 01:51:02 +0100

    3. SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-    Prevent SQL Injection use "execute" with %s
-"""
-
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    sql = """
-    SELECT id, name FROM states WHERE
-    name COLLATE latin1_general_cs LIKE
-    %s ORDER BY id ASC;
-    """
-    cur.execute(sql, (argv[4], ))
-
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
