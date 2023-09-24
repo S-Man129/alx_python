96529e37ab1f67c4e2e6eb1c2603c514df636e09
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-24 01:37:42 +0100

    Fetch all states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,11 @@ if __name__ == '__main__':
     state_name = sys.argv[4]
 
     # Create a connection to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
