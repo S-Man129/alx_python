fc700cbfd6652f0345aed26564972b7880fb4ab0
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-24 14:46:22 +0100

    model state fetch

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,12 +5,14 @@ import sys
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
-#creating a class create engine
 
 if __name__ == "__main__":
+    # Create an SQLAlchemy engine to connect to the database
+    # The provided arguments are assumed to be: username, password, and database name
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    #creating a class create engine
+    
+    # Create the tables in the database based on the defined models (declarative_base)
     Base.metadata.create_all(engine)
-    #creating a class create engine
+    
