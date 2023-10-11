b771f4d08e8653bce26b51ead9eed7209f16c5dc
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-11 13:39:01 +0100

    updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,31 @@
 import requests
-import sys
 
-def get_employee_todo_progress(employee_id):
-    # Define the base URL for the API
+def get_employee_todo_list_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
+    employee_endpoint = f"{base_url}/users/{employee_id}"
+    todo_endpoint = f"{base_url}/users/{employee_id}/todos"
 
-    # Fetch employee details
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    
-    # Fetch TODO list for the employee
-    todo_url = f"{base_url}/users/{employee_id}/todos"
-    todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
-    
-    # Count the number of completed and total tasks
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task['completed'])
+    try:
+        employee_response = requests.get(employee_endpoint)
+        employee_data = employee_response.json()
+        employee_name = employee_data.get("name")
 
-    # Print employee TODO list progress
-    print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
+        todo_response = requests.get(todo_endpoint)
+        todo_data = todo_response.json()
 
-    # Print titles of completed tasks
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
+        total_tasks = len(todo_data)
+        completed_tasks = sum(1 for task in todo_data if task["completed"])
+
+        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+
+        for idx, task in enumerate(todo_data, start=1):
+            if task["completed"]:
+                print(f"Task {idx} Formatting: OK")
+                print(f"\t{task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching data: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python todo_progress.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    employee_id = int(input("Enter employee ID: "))
+    get_employee_todo_list_progress(employee_id)
