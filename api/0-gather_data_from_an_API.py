68f266bcb0463bed9e7c1e8a3eb6bcfc89206d46
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-11 13:40:56 +0100

    corrected

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,34 @@
 import requests
 
 def get_employee_todo_list_progress(employee_id):
+    # Define the API endpoints
     base_url = "https://jsonplaceholder.typicode.com"
     employee_endpoint = f"{base_url}/users/{employee_id}"
     todo_endpoint = f"{base_url}/users/{employee_id}/todos"
 
     try:
+        # Fetch employee information
         employee_response = requests.get(employee_endpoint)
         employee_data = employee_response.json()
         employee_name = employee_data.get("name")
 
+        # Fetch TODO list
         todo_response = requests.get(todo_endpoint)
         todo_data = todo_response.json()
 
+        # Count the number of completed and total tasks
         total_tasks = len(todo_data)
         completed_tasks = sum(1 for task in todo_data if task["completed"])
 
+        # Print employee TODO list progress
         print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
 
-        for idx, task in enumerate(todo_data, start=1):
+        # Print the titles of completed tasks
+        task_count = 0
+        for task in todo_data:
             if task["completed"]:
-                print(f"Task {idx} Formatting: OK")
+                task_count += 1
+                print(f"Task {task_count} Formatting: OK")
                 print(f"\t{task['title']}")
 
     except requests.exceptions.RequestException as e:
