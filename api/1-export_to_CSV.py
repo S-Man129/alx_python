d48424273467368fed44fda8c46136032dc5d941
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-12 11:41:26 +0100

    Python API task1 edited

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-'''Using what you did in the task #0, extend your Python script to export data
-in the CSV format.'''
 
 import csv
 import requests
@@ -11,8 +9,7 @@ base_url = 'https://jsonplaceholder.typicode.com/'
 
 def do_request():
     ''' request '''
-
-    if not len(sys.argv):
+    if len(sys.argv) < 2:
         return print('USAGE:', __file__, '<employee id>')
     eid = sys.argv[1]
     try:
@@ -31,17 +28,26 @@ def do_request():
     if response.status_code != 200:
         return print('Error: status_code:', response.status_code)
     todos = response.json()
+
     user_todos = [todo for todo in todos
                   if todo.get('userId') == user.get('id')]
     completed = [todo for todo in user_todos if todo.get('completed')]
+    print('Employee', user.get('name'),
+          'is done with tasks({}/{}):'.
+          format(len(completed), len(user_todos)))
+    [print('\t', todo.get('title')) for todo in completed]
 
-    with open(eid + '.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, lineterminator='\n',
-                            quoting=csv.QUOTE_ALL)
-        [writer.writerow(['{}'.format(field) for field in
-                          (todo.get('userId'), user.get('username'),
-                           todo.get('completed'), todo.get('title'))])
-         for todo in user_todos]
+    # Create a CSV file for the user
+    csv_filename = f'{user.get("id")}.csv'
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for todo in user_todos:
+            csv_writer.writerow([user.get("id"), user.get("name"), todo.get("completed"), todo.get("title")])
+
+    print(f'CSV data has been saved to {csv_filename}')
+
 
 if __name__ == '__main__':
     do_request()
