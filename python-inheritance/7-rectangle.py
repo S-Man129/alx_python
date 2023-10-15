d2fd655861263d8a36b3a7e2ccf0225c24f84f12
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-15 21:21:48 +0100

    7 rectangle corrected

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
-""" Class Rectangle """
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+"""Rectangle class Module"""
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class Rectangle """
+    """Rectangle class"""
     def __init__(self, width, height):
-        """ init method """
+        """Initilize rectangle method"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        """ return area """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ __str__ method """
-        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
-    
-    def __init_subclass__(cls):
-        cls.__init_subclass__ = lambda *args, **kwargs: None
-
-    def __dir__(self):
-        # Get the default list of attributes and methods
-        default_dir = super().__dir__()
-
-        # Exclude '__init_subclass__' if it's in the default_dir
-        if '__init_subclass__' in default_dir:
-            default_dir.remove('__init_subclass__')
-
-        return default_dir
