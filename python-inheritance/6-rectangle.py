bb580533e984240856da14b6b7bcade99bac3749
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-15 20:51:34 +0100

    6 rectangle added

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-""" Class Rectangle """
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-class BaseGeometryMeta(type):
-    """BaseGeometry class"""
-    def __init_subclass__(cls):
-        cls.__init_subclass__ = lambda *args, **kwargs: None
-
-class BaseGeometry(metaclass=BaseGeometryMeta):
-    """BaseGeometry class"""
-    def __dir__(self):
-        default_dir = super().__dir__()
-        default_dir.remove('__init_subclass__')
-        return default_dir
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
+        """Initilize rectangle method"""
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        return self.__width * self.__height
-
