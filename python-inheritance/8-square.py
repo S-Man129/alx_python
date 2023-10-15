93dcd06d6bb23b1217ec096cc5c29668b4932648
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-15 21:28:46 +0100

    8 Square correct

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """defines class Square that inherits from Rectangle"""
 
-
 Rectangle = __import__('7-rectangle').Rectangle
 
 
@@ -17,16 +16,3 @@ class Square(Rectangle):
     def area(self):
         """returns area of square"""
         return (self.__size * self.__size)
-    
-    def __init_subclass__(cls):
-        cls.__init_subclass__ = lambda *args, **kwargs: None
-
-    def __dir__(self):
-        # Get the default list of attributes and methods
-        default_dir = super().__dir__()
-
-        # Exclude '__init_subclass__' if it's in the default_dir
-        if '__init_subclass__' in default_dir:
-            default_dir.remove('__init_subclass__')
-
-        return default_dir
