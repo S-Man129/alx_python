93fb6cedeeb147d02e6221aa336e3de4ebf24191
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-16 07:35:32 +0100

    api task #1 newadded

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Exports data in the CSV format"""
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
 #     import csv
 #     import requests
@@ -22,48 +22,48 @@ if __name__ == "__main__":
 #                 writer.writerow([userId, name, str(task.get('completed')),
 #                                  task.get('title')])
 
-    import csv
-    import requests
-    import sys
+import csv
+import requests
+import sys
 
-    def main():
-        if len(sys.argv) != 2:
-            print("Usage: python script_name.py <user_id>")
-            return
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <user_id>")
+        return
 
-        user_id = sys.argv[1]
-        user_endpoint = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-        todos_endpoint = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
+    user_id = sys.argv[1]
+    user_endpoint = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    todos_endpoint = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
 
-        try:
-            # Fetch user information
-            user_data = requests.get(user_endpoint).json()
-            name = user_data.get('name')
+    try:
+        # Fetch user information
+        user_data = requests.get(user_endpoint).json()
+        name = user_data.get('name')
 
-            # Fetch TODO list
-            todos_data = requests.get(todos_endpoint).json()
+        # Fetch TODO list
+        todos_data = requests.get(todos_endpoint).json()
 
-            # Create a CSV file for the user
-            csv_filename = f'{user_id}.csv'
-            with open(csv_filename, 'w', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        # Create a CSV file for the user
+        csv_filename = f'{user_id}.csv'
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-                for todo in todos_data:
-                    csv_writer.writerow(user_id, name, todo['completed'], todo['title'])
+            for todo in todos_data:
+                csv_writer.writerow(user_id, name, todo['completed'], todo['title'])
 
-            print(f'CSV data has been saved to {csv_filename}')
+        print(f'CSV data has been saved to {csv_filename}')
 
-            # Check the number of tasks in the CSV
-            with open(csv_filename, 'r') as csv_file:
-                num_tasks_in_csv = sum(1 for _ in csv_file) - 1  # Subtract 1 for the header
-            if num_tasks_in_csv == len(todos_data):
-                print('Correct number of tasks in CSV')
-            else:
-                print('Incorrect number of tasks in CSV')
+        # Check the number of tasks in the CSV
+        with open(csv_filename, 'r') as csv_file:
+            num_tasks_in_csv = sum(1 for _ in csv_file) - 1  # Subtract 1 for the header
+        if num_tasks_in_csv == len(todos_data):
+            print('Correct number of tasks in CSV')
+        else:
+            print('Incorrect number of tasks in CSV')
 
-        except requests.exceptions.RequestException as e:
-            print(f'An error occurred while fetching data: {e}')
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred while fetching data: {e}')
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
