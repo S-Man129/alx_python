96b3a06ace96c5feb904edfb3e6a9f954848357f
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-16 07:47:24 +0100

    api task #1 correct

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,46 +5,30 @@ import csv
 import requests
 import sys
 
-def fetch_user_info(user_id):
-    user_endpoint = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    user_data = requests.get(user_endpoint).json()
-    return user_data.get('name')
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-def fetch_user_tasks(user_id):
-    todos_endpoint = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
-    return requests.get(todos_endpoint).json()
 
-def export_to_csv(user_id, name, todos_data):
-    csv_filename = f'{user_id}.csv'
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for todo in todos_data:
-            csv_writer.writerow([user_id, name, todo['completed'], todo['title']])
-    
-    print(f'CSV data has been saved to {csv_filename}')
+def user_info(id):
+    """ Check user information """
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <user_id>")
-        return
+    total_tasks = 0
+    response = requests.get(todos_url).json()
+    for i in response:
+        if i['userId'] == id:
+            total_tasks += 1
 
-    user_id = sys.argv[1]
+    num_lines = 0
+    with open(str(id) + ".csv", 'r') as f:
+        for line in f:
+            if not line == '\n':
+                num_lines += 1
 
-    try:
-        name = fetch_user_info(user_id)
-        todos_data = fetch_user_tasks(user_id)
-        export_to_csv(user_id, name, todos_data)
+    if total_tasks == num_lines:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
 
-        # Check the number of tasks in the CSV
-        num_tasks_in_csv = len(todos_data)
-        if num_tasks_in_csv == len(todos_data):
-            print('Correct number of tasks in CSV')
-        else:
-            print('Incorrect number of tasks in CSV')
-
-    except requests.exceptions.RequestException as e:
-        print(f'An error occurred while fetching data: {e}')
 
 if __name__ == "__main__":
-    main()
+    user_info(int(sys.argv[1]))
