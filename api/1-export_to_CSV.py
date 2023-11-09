7e96959ddde0ee631618b7b02402610a39cc3398
S-Man129 <33316848+S-Man129@users.noreply.github.com>
2023-11-09 12:18:13 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
+"""
+Python script to export data in the CSV format.
+"""
 
 import csv
 import requests
 import sys
 
+
+def export_to_CSV(user_id):
+    employee_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}".format(user_id)
+    ).json()[0]["username"]
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    ).json()
+
+    tasks_data = []
+
+    for task in tasks:
+        tasks_data.append(
+            [
+                user_id,
+                employee_name,
+                task["completed"],
+                task["title"],
+            ]
+        )
+
+    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(tasks_data)
+
+
 if __name__ == "__main__":
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-
-    name = user.json().get('name')
-
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
-    completed = 0
-
-    # Create a list to store tasks for CSV export
-    tasks_for_csv = []
-
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
-
-            # Add the task to the list for CSV export
-            tasks_for_csv.append({
-                "USER_ID": userId,
-                "USERNAME": name,
-                "TASK_COMPLETED_STATUS": task.get('completed'),
-                "TASK_TITLE": task.get('title')
-            })
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
-
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
-
-    # Export data to CSV
-    with open(f'{userId}.csv', 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerows(tasks_for_csv)
+    export_to_CSV(sys.argv[1])
